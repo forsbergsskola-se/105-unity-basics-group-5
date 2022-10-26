6f829f296e662298082c4a05ce8f177e2b205dbb
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2022-10-26 09:22:35 +0200

    Add Drag Brick - 2.3 Event Functions finished

--- SmallTheftAuto/Assets/Scripts/Brick.cs
+++ SmallTheftAuto/Assets/Scripts/Brick.cs
@@ -1,12 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
 
-public class Brick : MonoBehaviour
+public class Brick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     void Start()
     {
         transform.position += transform.right * Random.Range(-0.1f, 0.1f);
     }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        Plane plane = new Plane(Vector3.up, Vector3.up * eventData.pointerPressRaycast.worldPosition.y);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            transform.position = ray.GetPoint(distance);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+    
 }
