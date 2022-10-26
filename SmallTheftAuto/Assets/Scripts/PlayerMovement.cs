090c4e37490d0c95a7c17c338a3eead8ec2bfcfb
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2022-10-26 09:52:07 +0200

    Add playerMovement - 2.4 Event Functions finished

---
+++ SmallTheftAuto/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        /*
+        if (Input.GetKey(KeyCode.W)) 
+            transform.Translate(0f, 0f, 1f * Time.deltaTime) ;
+        if (Input.GetKey(KeyCode.S)) 
+            transform.Translate(0f, 0f, -1f * Time.deltaTime) ;
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-1f * Time.deltaTime, 0f, 0f ) ;
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(1f * Time.deltaTime, 0f, 0f) ;
+        */
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(horizontal * Time.deltaTime, 0f, 0f) ;
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(0f, 0f, vertical * Time.deltaTime) ;
+        
+       
+
+
+    }
+}
